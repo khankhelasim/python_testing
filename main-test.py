480d0e8d7837f5f59717bdf6618856ca033ec097
khankhelasim <khankhelasim@gmail.com>
2025-04-13 18:35:31 +0500

    first commit

---
+++ main-test.py
@@ -0,0 +1,9 @@
+from main import Add
+def test_Add():
+    assert Add(2, 3) == 5
+    print("Test passed")
+
+if __name__ == "__main__":
+    test_Add()
+    print("All tests passed")
+   
---
+++ main.py
@@ -0,0 +1,7 @@
+def Add (a,b):
+    return a + b
+def green ():
+    print("Welcome")
+
+if __name__ == "__main__":
+    green()
